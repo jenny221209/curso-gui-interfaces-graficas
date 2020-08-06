b1b6b03bc3c0d67c3b870140cf92699f617ca5ba
jenny alexandra gomez parra <jenny16parr@gmail.com>
2020-08-06 08:27:24 -0500

    New: boton

--- gui-bootstrap-react-js/src/index.js
+++ gui-bootstrap-react-js/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Estilos Bootstrap 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
